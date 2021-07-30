1680eed6e468c00e6b7a5afe06afd78f53833a66
Takuya <dyhpaax@gmail.com>
2021-07-30 14:41:46 +0900

    name.email.passwordバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
+  validates :password, length: { minimum: 8. maximum: 32},
+                       format: {with: /\A[0-9a-zA-Z]+\z/i }
 
   has_secure_password
 
