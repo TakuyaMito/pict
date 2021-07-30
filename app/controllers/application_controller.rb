c6eab910d9e08d7e008b06b236ac8664da6405cc
Takuya <dyhpaax@gmail.com>
2021-07-31 08:44:46 +0900

    写真投稿機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+  before_action :login_required
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
   end
 end
